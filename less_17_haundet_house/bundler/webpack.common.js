ed7a7f2fe860bfe82fe418d327089bee818d6ae2
Scherbachenya, Igor <evgeniionegin791@gmail.com>
2022-04-17 21:38:37 +0300

    less 17 house with ghosts

---
+++ less_17_haundet_house/bundler/webpack.common.js
@@ -0,0 +1,89 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, '../src/script.ts'),
+    output: {
+        hashFunction: 'xxhash64',
+        filename: 'bundle.[contenthash].js',
+        path: path.resolve(__dirname, '../dist'),
+        clean: true
+    },
+    devtool: 'source-map',
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, '../static') }
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../src/index.html'),
+            minify: true
+        }),
+        new MiniCSSExtractPlugin()
+    ],
+    module: {
+        rules:
+        [
+            {
+                test: /\.node$/,
+                use: "node-loader",
+            },
+            // HTML
+            {
+                test: /\.(html)$/,
+                use:
+                [
+                    'html-loader'
+                ]
+            },
+
+            // JS
+            {
+                test: /\.(ts)x?$/,
+                exclude: /node_modules|test/,
+                use: "babel-loader",
+            },
+
+            // CSS
+            {
+                test: /\.css$/,
+                use:
+                [
+                    MiniCSSExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+
+            // Images
+            {
+                test: /\.(jpg|png|gif|svg)$/,
+                type: 'asset/resource',
+                generator:
+                {
+                    filename: 'assets/images/[hash][ext]'
+                }
+            },
+
+            // Fonts
+            {
+                test: /\.(ttf|eot|woff|woff2)$/,
+                type: 'asset/resource',
+                generator:
+                {
+                    filename: 'assets/fonts/[hash][ext]'
+                }
+            }
+        ]
+    },
+
+    resolve: {
+        modules: [
+            path.resolve(__dirname, '../node_modules'),
+            path.resolve(__dirname, '../src')
+        ],
+        extensions: [".js", ".ts", ".html", ".css", ".json"]
+    },
+}
